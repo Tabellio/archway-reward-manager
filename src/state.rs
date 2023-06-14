2896a04314a4d6a399b8a08d96c4e6b96b4c0112
findolor <anakisci@gmail.com>
2023-06-14 12:39:46 +0300

    Add simple state

--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,14 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Decimal};
+use cw_storage_plus::Map;
 
+// Share consists of an address and a percentage
+// Address is the address of the recipient
+// Percentage is the percentage of the total amount to be sent to the recipient
+#[cw_serde]
+pub struct Share {
+    recipient: String,
+    percentage: Decimal,
+}
+
+pub const SHARES: Map<Addr, Share> = Map::new("shares");
