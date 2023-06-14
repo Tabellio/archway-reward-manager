0f87e507efa7aaf1081433b384355dee8fd5a055
findolor <anakisci@gmail.com>
2023-06-14 12:52:15 +0300

    Add instantiate message

--- src/contract.rs
+++ src/contract.rs
@@ -1,27 +1,54 @@
+use archway_bindings::{ArchwayQuery, ArchwayResult};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{Binary, Decimal, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-
+use crate::state::{Config, CONFIG, SHARES};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:archway-reward-manager";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    deps: DepsMut,
+    deps: DepsMut<ArchwayQuery>,
     _env: Env,
-    _info: MessageInfo,
-    _msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> ArchwayResult<ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
-    unimplemented!()
+    // Save the sender as the admin
+    let config = Config {
+        admin: info.sender.clone(),
+        mutable: msg.mutable,
+    };
+    CONFIG.save(deps.storage, &config)?;
+
+    // Total percentage of shares
+    // Used to validate that the total percentage does not exceed 100%
+    let mut total_percentage = Decimal::zero();
+
+    // Processing each share
+    for share in msg.shares {
+        // Validating the recipient address
+        let recipient = deps.api.addr_validate(&share.recipient)?;
+
+        // Adding the percentage to the total percentage
+        total_percentage += share.percentage;
+
+        if total_percentage > Decimal::one() {
+            return Err(ContractError::PercentageLimitExceeded {});
+        }
+
+        // Saving the share
+        SHARES.save(deps.storage, recipient, &share)?;
+    }
+
+    Ok(Response::new().add_attribute("admin", info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -30,11 +57,11 @@ pub fn execute(
     _env: Env,
     _info: MessageInfo,
     _msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
+) -> ArchwayResult<ContractError> {
     unimplemented!()
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+pub fn query(_deps: Deps<ArchwayQuery>, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
     unimplemented!()
 }
