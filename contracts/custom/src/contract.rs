5bd4e3ec1121bf4cd9be5582612ed289b5d8261a
findolor <anakisci@gmail.com>
2023-06-22 10:14:40 +0300

    Update custom contract and add README

--- contracts/custom/src/contract.rs
+++ contracts/custom/src/contract.rs
@@ -1,3 +1,4 @@
+use archway_bindings::{ArchwayMsg, ArchwayQuery, ArchwayResult};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
@@ -12,27 +13,56 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    deps: DepsMut,
+    deps: DepsMut<ArchwayQuery>,
     _env: Env,
     _info: MessageInfo,
     _msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
+) -> ArchwayResult<ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
-    unimplemented!()
+    Ok(Response::new())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    deps: DepsMut,
-    _env: Env,
+    deps: DepsMut<ArchwayQuery>,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
-    match msg {}
+) -> ArchwayResult<ContractError> {
+    match msg {
+        // ... other execute messages
+        ExecuteMsg::UpdateRewardMetadata {
+            owner_address,
+            rewards_address,
+        } => execute_update_reward_metadata(deps, env, info, owner_address, rewards_address),
+    }
+}
+
+// ... other execute methods
+
+// TODO: Add this execute method
+// This will update the owner and rewards address on this contract
+fn execute_update_reward_metadata(
+    _deps: DepsMut<ArchwayQuery>,
+    _env: Env,
+    _info: MessageInfo,
+    owner_address: Option<String>,
+    rewards_address: Option<String>,
+) -> ArchwayResult<ContractError> {
+    // ... write custom logic if needed
+
+    let msg = ArchwayMsg::UpdateContractMetadata {
+        owner_address,
+        rewards_address,
+    };
+
+    Ok(Response::new()
+        .add_message(msg)
+        .add_attribute("method", "update_rewards_address"))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+pub fn query(_deps: Deps<ArchwayQuery>, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
     unimplemented!()
 }
