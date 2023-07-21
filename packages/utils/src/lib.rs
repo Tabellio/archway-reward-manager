45201d1d30e0a68a3c2c693cf56f0a410e8ece1c
findolor <anakisci@gmail.com>
2023-07-21 09:25:38 +0300

    Add pantheon packages

---
+++ packages/utils/src/lib.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Decimal;
+
+// Share consists of an address and a percentage
+// Address is the address of the recipient
+// Percentage is the percentage of the total amount to be sent to the recipient
+#[cw_serde]
+pub struct Share {
+    pub recipient: String,
+    pub percentage: Decimal,
+}
